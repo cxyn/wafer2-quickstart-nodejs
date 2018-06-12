d50c140801cee3ef0d42f08e043752411a5c0c20
WangYunxiao <wangyx@htd.cn>
2018-06-12 18:03:41 +0800

    test

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,5 +1,9 @@
+const { mysql } = require('../qcloud');
+
+// let t = mysql('fc').select('*').where({ id: 1 });
+
 module.exports = ctx => {
   ctx.state.data = {
-    msg: '花港FC'
+    msg: 'fuck'
   }
 }
